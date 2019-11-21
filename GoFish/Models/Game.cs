8a578023c2814ed8162d073baea256fb38aa92ff
lbolin <lbolin@users.noreply.github.com>
2019-11-20 23:23:13 -0600

    Added Game View model code

--- GoFish/Models/Game.cs
+++ GoFish/Models/Game.cs
@@ -8,5 +8,30 @@ namespace GoFish.Models
 {
     class Game
     {
+        private List<Hand> hands;
+
+        public Game()
+        {
+            Hand opponent= new Hand();
+            Hand player = new Hand();
+            Hand deck = new Hand(true);
+
+            hands.Add(deck);
+            hands.Add(player);
+            hands.Add(opponent);
+
+        }
+
+        public Hand getHand(int index)
+        {
+            if (index != 0 && index != 1 && index != 2)
+            {
+                return null;
+            }
+            else
+            {
+                return hands[index];
+            }
+        }
     }
 }

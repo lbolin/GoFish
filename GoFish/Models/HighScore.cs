91ac65973a6599c43ce14b73a621cf9dc854b012
lbolin <lbolin@users.noreply.github.com>
2019-12-04 15:39:19 -0600

    Revert "Revert "Revert "Working on databinding"""
    This reverts commit 384dad61a7349be9eece7572e86cc258bddd77e6.

--- GoFish/Models/HighScore.cs
+++ GoFish/Models/HighScore.cs
@@ -1,31 +1,22 @@
-﻿using GoFish.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GoFish.Models
 {
-   
-    class HighScore {
+    class HighScore
+    {
         public string Name { get; }
         public int Books { get; }
         public int Time { get; }
 
-    
-
-
-
-        public HighScore(string name, int books, int time)
+        public HighScore(string name,int books,int time)
         {
             Name = name;
             Books = books;
             Time = time;
         }
-
-        
     }
 }
